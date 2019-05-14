0cdaac471df4bf168271b5a05974dd1fbe56f3eb
Pierre Aubert <aubertp7@gmail.com>
2019-05-14 09:48:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,11 @@ setup(
     long_description_content_type='text/markdown',
     entry_points = entry_points,
     install_requires=[
-        'tables',
-        'ctapipe',
+        'numpy>=1.14.0', 
+	'tables>=3.4.4',
+	'pyhessio @ https://api.github.com/repos/cta-observatory/pyhessio/tarball/v2.1.1',
+	'ctapipe @ https://api.github.com/repos/cta-observatory/ctapipe/tarball/v0.6.2',
+	'ctapipe-extra @ https://api.github.com/repos/cta-observatory/ctapipe-extra/tarball/v0.2.16'
     ],
     tests_require=['pytest'],
     setup_requires=['pytest_runner'],
