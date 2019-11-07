16a18a6ae5a30424ef697908fc706ab4532326a2
Pierre Aubert <aubertp7@gmail.com>
2019-11-07 16:24:54 +0100

    Fix NONSENCE try except git status git status !

--- ctapipe_io_mchdf5/tools/get_telescope_info.py
+++ ctapipe_io_mchdf5/tools/get_telescope_info.py
@@ -33,65 +33,64 @@ TELINFO_NBEVENT = 16
 TEL_INFOR_CAMERA_ROTATION = 17
 TEL_INFOR_PIX_ROTATION = 18
 
-try:
-	def getTelescopeInfoFromEvent(inputFileName, max_nb_tel):
-		'''
-		Get the telescope information from the event
-		Parameters:
-		-----------
-			inputFileName : name of the input file to be used
-			max_nb_tel : maximum number of telescope in the simulation
-		Return:
-		-------
-			tuple of (dictionnnary which contains the telescope informations (ref_shape, nb_slice, ped, gain) with telescope id as key, and the number of events in the file
-		'''
-		telescope_info = dict() # Key is tel id, value (ref_shape, slice, ped, gain, telType, focalLen, tabPixelX, tabPixelY, nbMirror)
-		nbEvent = 0
-		with event_source(inputFileName) as source:
-			dicoTelInfo = None
-			posTelX = None
-			posTelY = None
-			posTelZ = None
-			for evt in source:
-				nbEvent += 1
-				if dicoTelInfo is None:
-					dicoTelInfo = evt.inst.subarray.tel
-					posTelX = np.asarray(evt.inst.subarray.tel_coords.x, dtype=np.float32)
-					posTelY = np.asarray(evt.inst.subarray.tel_coords.y, dtype=np.float32)
-					posTelZ = np.asarray(evt.inst.subarray.tel_coords.z, dtype=np.float32)
-				for tel_id in evt.r0.tels_with_data:
-					if not tel_id in telescope_info:
-						ref_shape = evt.mc.tel[tel_id].reference_pulse_shape
-						nb_slice = evt.r0.tel[tel_id].waveform.shape[2]
-						nbGain = evt.r0.tel[tel_id].waveform.shape[0]
-						nbPixel = evt.r0.tel[tel_id].waveform.shape[1]
-						ped = evt.mc.tel[tel_id].pedestal
-						gain =  evt.mc.tel[tel_id].dc_to_pe
-						cameraRotation = evt.inst.subarray.tel[tel_id].camera.pix_rotation
-						pixRotation = evt.inst.subarray.tel[tel_id].camera.cam_rotation
-						
-						telInfo = dicoTelInfo[tel_id]
-						telType = np.uint64(getCameraTypeFromName(telInfo.camera.cam_id))
-						focalLen = np.float32(telInfo.optics.equivalent_focal_length.value)
-						
-						tabPixelX = np.asarray(telInfo.camera.pix_x, dtype=np.float32)
-						tabPixelY = np.asarray(telInfo.camera.pix_y, dtype=np.float32)
-						
-						nbMirror = np.uint64(telInfo.optics.num_mirrors)
-						nbMirrorTiles = np.uint64(telInfo.optics.num_mirror_tiles)
-						mirrorArea = np.uint64(telInfo.optics.mirror_area.value)
-						
-						telX = posTelX[tel_id - 1]
-						telY = posTelY[tel_id - 1]
-						telZ = posTelZ[tel_id - 1]
-						
-						telescope_info[tel_id] = [ref_shape, nb_slice, ped, gain, telType, focalLen, tabPixelX, tabPixelY, nbMirror,
-										telX, telY, telZ, nbMirrorTiles, mirrorArea, nbGain, nbPixel, 0, cameraRotation, pixRotation]
-					else:
-						telescope_info[tel_id][TELINFO_NBEVENT] += 1
-		return telescope_info, nbEvent
-except:
-	pass
+
+def getTelescopeInfoFromEvent(inputFileName, max_nb_tel):
+	'''
+	Get the telescope information from the event
+	Parameters:
+	-----------
+		inputFileName : name of the input file to be used
+		max_nb_tel : maximum number of telescope in the simulation
+	Return:
+	-------
+		tuple of (dictionnnary which contains the telescope informations (ref_shape, nb_slice, ped, gain) with telescope id as key, and the number of events in the file
+	'''
+	telescope_info = dict() # Key is tel id, value (ref_shape, slice, ped, gain, telType, focalLen, tabPixelX, tabPixelY, nbMirror)
+	nbEvent = 0
+	with event_source(inputFileName) as source:
+		dicoTelInfo = None
+		posTelX = None
+		posTelY = None
+		posTelZ = None
+		for evt in source:
+			nbEvent += 1
+			if dicoTelInfo is None:
+				dicoTelInfo = evt.inst.subarray.tel
+				posTelX = np.asarray(evt.inst.subarray.tel_coords.x, dtype=np.float32)
+				posTelY = np.asarray(evt.inst.subarray.tel_coords.y, dtype=np.float32)
+				posTelZ = np.asarray(evt.inst.subarray.tel_coords.z, dtype=np.float32)
+			for tel_id in evt.r0.tels_with_data:
+				if not tel_id in telescope_info:
+					ref_shape = evt.mc.tel[tel_id].reference_pulse_shape
+					nb_slice = evt.r0.tel[tel_id].waveform.shape[2]
+					nbGain = evt.r0.tel[tel_id].waveform.shape[0]
+					nbPixel = evt.r0.tel[tel_id].waveform.shape[1]
+					ped = evt.mc.tel[tel_id].pedestal
+					gain =  evt.mc.tel[tel_id].dc_to_pe
+					cameraRotation = evt.inst.subarray.tel[tel_id].camera.pix_rotation
+					pixRotation = evt.inst.subarray.tel[tel_id].camera.cam_rotation
+					
+					telInfo = dicoTelInfo[tel_id]
+					telType = np.uint64(getCameraTypeFromName(telInfo.camera.cam_id))
+					focalLen = np.float32(telInfo.optics.equivalent_focal_length.value)
+					
+					tabPixelX = np.asarray(telInfo.camera.pix_x, dtype=np.float32)
+					tabPixelY = np.asarray(telInfo.camera.pix_y, dtype=np.float32)
+					
+					nbMirror = np.uint64(telInfo.optics.num_mirrors)
+					nbMirrorTiles = np.uint64(telInfo.optics.num_mirror_tiles)
+					mirrorArea = np.uint64(telInfo.optics.mirror_area.value)
+					
+					telX = posTelX[int(tel_id - 1)]
+					telY = posTelY[int(tel_id - 1)]
+					telZ = posTelZ[int(tel_id - 1)]
+					
+					telescope_info[tel_id] = [ref_shape, nb_slice, ped, gain, telType, focalLen, tabPixelX, tabPixelY, nbMirror,
+									telX, telY, telZ, nbMirrorTiles, mirrorArea, nbGain, nbPixel, 0, cameraRotation, pixRotation]
+				else:
+					telescope_info[tel_id][TELINFO_NBEVENT] += 1
+	return telescope_info, nbEvent
+
 
 
 def checkIsSimulationFile(telInfo_from_evt):
